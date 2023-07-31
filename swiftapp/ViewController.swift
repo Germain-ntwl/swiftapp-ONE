417f0f3ecd7a975a8967e28251212385e0831c01
hurricane <ntwlgermain@gmail.com>
2023-07-31 22:55:27 +0200

    swiftapp 1 updated

---
+++ swiftapp/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  swiftapp
+//
+//  Created by hurricane on 8/9/22.
+//  Copyright © 2022 germain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad(password: 1234567890)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
+<script>
+// Modal Image Gallery
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+}
+
+// Change style of navbar on scroll
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+</script>
